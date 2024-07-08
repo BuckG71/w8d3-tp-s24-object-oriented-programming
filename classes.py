69f75f0afe59c11df9081ee8cda92d54b46e81a4
Bryan Garrett <67705098+BuckG71@users.noreply.github.com>
2024-07-07 21:08:49 -0500

    Created stub for parent class "Shape"

---
+++ classes.py
@@ -0,0 +1,19 @@
+"""This file contains all of the classes required for the program."""
+
+class Shape:
+    def __init__(self, color) -> None:
+        self.color = color
+    
+    def calculate_area(self):
+        pass
+
+    def calculate_perimeter(self):
+        pass
+
+    def get_color(self):
+        pass
+
+    def set_color(self):
+        pass
+
+    
