84916f123771d89c71da15c2868e4824519d090f
Bryan Garrett <67705098+BuckG71@users.noreply.github.com>
2024-07-07 21:24:11 -0500

    implemented "NotImplementedError" and made Shape an abstract class using abc + @abstractmethod decorator

--- classes.py
+++ classes.py
@@ -1,19 +1,27 @@
 """This file contains all of the classes required for the program."""
+from abc import ABC, abstractmethod
 
-class Shape:
+class Shape(ABC):
+    """The parent class of all shapes"""
     def __init__(self, color) -> None:
         self.color = color
-    
+
+    @abstractmethod
     def calculate_area(self):
-        pass
+        """An abstract method to calculate the area of the shape"""
+        raise NotImplementedError("Subclasses should implement this method")
 
+    @abstractmethod
     def calculate_perimeter(self):
-        pass
+        """An abstract method to calculate the perimeter of the shape"""
+        raise NotImplementedError("Subclasses should implement this method")
 
+    @abstractmethod
     def get_color(self):
-        pass
+        """An abstract method to get the color of the shape"""
+        raise NotImplementedError("Subclasses should implement this method")
 
+    @abstractmethod
     def set_color(self):
-        pass
-
-    
+        """An abstract method to change the color of the shape"""
+        raise NotImplementedError("Subclasses should implement this method")
